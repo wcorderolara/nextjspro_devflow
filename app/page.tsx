9632c307488e76324fc9e2a9d1696b898d7912b3
Walter Cordero <walter.cordero@theksquaregroup.com>
2025-02-12 11:54:21 -0600

    setting up ESLINT

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-
-
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl text-violet-700 font-black">Welcome to the world of Next.js</h1>
+      <h1 className="text-3xl text-violet-700 font-black">
+        Welcome to the world of Next.js
+      </h1>
     </div>
   );
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,31 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import tailwind from "eslint-plugin-tailwindcss";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standard",
+    tailwind.configs["flat/recommended"],
+    "prettier"
+  ),
+  {
+    rules: {
+      "no-undef": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
