48e6adffc86e110f88bdf6b98c75f4b7a5b4bb9f
Walter Cordero <walter.cordero@theksquaregroup.com>
2025-02-17 10:03:52 -0600

    modify the css

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl text-violet-700 font-black">
+      <h1 className="text-3xl text-primay-100 font-black">
         Welcome to the world of Next.js
       </h1>
     </div>
