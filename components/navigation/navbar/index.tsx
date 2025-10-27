b0b4f16ded39972469b957d00a94f2425a4b3919
Walter Cordero <walter.cordero@wordly.ai>
2025-10-27 06:42:26 -0700

    Setting up Light & Dark Theme, Shacn, NavBar and Theme Toggle

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import Theme from "./Theme";
 
 const Navbar = () => {
   return (
-    <nav>
-      <h1>Navbar</h1>
+    <nav className="flex-between background-light900_dark200 shadow-ligth-300 fixed z-50 w-full gap-5 p-6 sm:px-12 dark:shadow-none">
+      <Link href="/" className="flex items-center gap-1">
+        <Image src="/images/site-logo.svg" width={23} height={23} alt="DevFlow Logo" />
+        <p className="h2-bold font-space-grotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+          Dev<span className="text-primary-500">Flow</span>
+        </p>
+      </Link>
+
+      <p>Global search</p>
+      <div className="flex-betwwen gap-5">
+        <Theme />
+      </div>
     </nav>
   );
 };
